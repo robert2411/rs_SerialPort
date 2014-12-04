ad68d868f76567d00b7c5ab098b420e4c4d276d1
Robert Stevens <robert@stevens.tk>
2014-12-04 12:12:43 +0100

    fully working

--- SerialPort.cpp
+++ SerialPort.cpp
@@ -10,6 +10,7 @@ SerialPort::SerialPort(const char* portName, bool debugModues)
 	
 	if(m_debugModus) printf("The serialPort class started in debug modus!\r\n");
 	
+	system("stty -F /dev/ttyUSB0 115200");
 	OpenPort();
 	//SetParams();
 	
@@ -32,39 +33,6 @@ int SerialPort::OpenPort()
 		if(m_debugModus) printf("Port 1 has been sucessfully opened and %d is the file description \r\n",m_fd1);
 		m_portIsOpen = true;
 	}
-}
-int SerialPort::SetParams()
-{
-	/* this function is based on code from http://stackoverflow.com/questions/18108932/linux-c-serial-port-reading-writing */
-	struct termios tty;
-	struct termios tty_old;
-	memset (&tty, 0, sizeof tty);
-
-	/* Save old tty parameters */
-	tty_old = tty;
-
-	/* Set Baud Rate */
-	cfsetospeed (&tty, (speed_t)B9600);
-	cfsetispeed (&tty, (speed_t)B9600);
-
-	/* Setting other Port Stuff */
-	tty.c_cflag     &=  ~PARENB;        // Make 8n1
-	tty.c_cflag     &=  ~CSTOPB;
-	tty.c_cflag     &=  ~CSIZE;
-	tty.c_cflag     |=  CS8;
-
-	tty.c_cflag     &=  ~CRTSCTS;       // no flow control
-	tty.c_cc[VMIN]      =   1;                  // read doesn't block
-	tty.c_cc[VTIME]     =   5;                  // 0.5 seconds read timeout
-	tty.c_cflag     |=  CREAD | CLOCAL;     // turn on READ & ignore ctrl lines
-
-	/* Make raw */
-	cfmakeraw(&tty);
-
-	/* Flush Port, then applies attributes */
-	tcflush( m_fd1, TCIFLUSH );
-	
-	return 0;
 }
 
 char SerialPort::ReadByte()
